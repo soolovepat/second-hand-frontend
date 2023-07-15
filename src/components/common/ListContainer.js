142f6f4b31101927ae62fe203126e40e19cf664c
Sujin Lee <soolovepat@naver.com>
2023-07-15 23:14:21 +0900

    fix: api

--- src/components/common/ListContainer.js
+++ src/components/common/ListContainer.js
@@ -12,7 +12,7 @@ const ListContainer = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await axios.get("http://localhost:4000/posts");
+        const response = await getPosts();
         setPostList(response.data);
       } catch (error) {
         console.error(error);
