5fa839711be15f5bb6d0f8ef699dcebc4cb5f429
Sujin Lee <soolovepat@naver.com>
2023-07-17 20:00:09 +0900

    feat: add write button

--- src/pages/pages/MainPage.js
+++ src/pages/pages/MainPage.js
@@ -3,14 +3,23 @@ import Header from "../../components/common/Header";
 import Footer from "../../components/common/Footer";
 import Banner from "../../components/common/Banner";
 import ListContainer from "../../containers/ListContainer";
+import Button from "../../common/Button";
+import { useNavigate } from "react-router-dom";
 
 const MainPage = () => {
+  const navigate = useNavigate();
+  const handleWrite = () => {
+    navigate("/write");
+  };
   return (
     <div>
       <Header />
       <Banner />
       <ListContainer />
       <Footer />
+      <Button size={"sm"} onClick={handleWrite}>
+        글쓰기
+      </Button>
     </div>
   );
 };
