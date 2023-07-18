856e9fd596610329abc8fb4fcc4aa6f5065f3da4
Sujin Lee <soolovepat@naver.com>
2023-07-18 16:25:35 +0900

    style: add logoGray

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { logoGray } from "../../assets/logo";
 
 const Footer = () => {
   return (
     <FooterBlock>
+      <Link to={"/"}>
+        <img className="logo-img" src={logoGray} />
+      </Link>
+
       <strong>어떤 내용을 넣을까?</strong>
       <ul>
         <li>Footer</li>
